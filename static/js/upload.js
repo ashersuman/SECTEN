9c3f767198430787e10e62a3da5cac2165d80b65
ashersuman <user11root@gmail.com>
2020-06-12 13:50:11 +0530

    Added Bid Upload Popup

---
+++ static/js/upload.js
@@ -0,0 +1,12 @@
+document.getElementById('upload-bid').addEventListener('click', 
+function () {
+    document.querySelector('html').style.overflow = 'hidden';
+    document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.querySelector('.close').addEventListener('click',
+function(){
+    document.querySelector('html').style.overflow = 'scroll';
+    document.querySelector('html').style.overflowX = 'hidden';
+    document.querySelector('.bg-modal').style.display = 'none';
+});
