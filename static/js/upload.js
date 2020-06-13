418cb650014363a5fa8b272d5bfd41780f99dd76
ashersuman <user11root@gmail.com>
2020-06-13 22:15:00 +0530

    File Upload Fix

--- static/js/upload.js
+++ static/js/upload.js
@@ -1,3 +1,10 @@
+$(document).ready(function(){
+    $('input[type="file"]').change(function(e){
+        var fileName = e.target.files[0].name;
+        $('.up-label').text('The file "' + fileName +  '" has been selected.');
+    });
+});
+
 document.getElementById('upload-bid').addEventListener('click', 
 function(){
     document.querySelector('html').style.overflow = 'hidden';
